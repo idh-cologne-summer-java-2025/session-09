6e9217be6d543267e30beb35fdefe910d5d50382
Jürgen Hermes <hermesj@uni-koeln.de>
2025-06-25 09:29:08 +0200

    Modified sort algorithms

--- src/SortAlgorthms/SortAlgorithms.java
+++ src/SortAlgorthms/SortAlgorithms.java
@@ -44,61 +44,78 @@ public class SortAlgorithms {
         mergeSort(arr, 0, arr.length - 1);
     }
 
-    public static void quickSort(int[] arr) {
-        quickSort(arr, 0, arr.length - 1);
-    }
-
-    private static void mergeSort(int[] arr, int left, int right) {
-        if (left < right) {
-            int mid = (left + right) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
+    public static void mergeSort(int[] arrToSort, int startIdx, int endIdx) {
+        if (startIdx >= endIdx) {
+            return;
         }
+
+        int midIdx = startIdx + (endIdx - startIdx) / 2;
+        mergeSort(arrToSort, startIdx, midIdx);
+        mergeSort(arrToSort, midIdx + 1, endIdx);
+        merge(arrToSort, startIdx, midIdx, endIdx);
     }
 
-    private static void merge(int[] arr, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
+    public static void merge(int[] arrToSort, int startIdx, int midIdx, int endIdx) {
+        int[] leftArr = new int[midIdx - startIdx + 1];
+        int[] rightArr = new int[endIdx - midIdx];
 
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[left + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[mid + 1 + j];
-
-        int i = 0, j = 0, k = left;
-        while (i < n1 && j < n2) {
-            arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+        for (int i = 0; i < leftArr.length; i++) {
+            leftArr[i] = arrToSort[startIdx + i];
         }
-        while (i < n1) arr[k++] = L[i++];
-        while (j < n2) arr[k++] = R[j++];
-    }
-
-    private static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+        for (int i = 0; i < rightArr.length; i++) {
+            rightArr[i] = arrToSort[midIdx + i + 1];
         }
-    }
 
-    private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1; // Index kleinerer Elemente
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                int tmp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tmp;
+        int leftArrIdx = 0, rightArrIdx = 0, sortedArrIdx = startIdx;
+        while ((leftArrIdx < leftArr.length) && (rightArrIdx < rightArr.length)) {
+            if (leftArr[leftArrIdx] < rightArr[rightArrIdx]) {
+                arrToSort[sortedArrIdx] = leftArr[leftArrIdx];
+                leftArrIdx += 1;
+            } else {
+                arrToSort[sortedArrIdx] = rightArr[rightArrIdx];
+                rightArrIdx += 1;
             }
+            sortedArrIdx += 1;
         }
-        int tmp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = tmp;
-        return i + 1;
+
+        while (leftArrIdx < leftArr.length) {
+            arrToSort[sortedArrIdx] = leftArr[leftArrIdx];
+            leftArrIdx += 1;
+            sortedArrIdx += 1;
+        }
+
+        while (rightArrIdx < rightArr.length) {
+            arrToSort[sortedArrIdx] = rightArr[rightArrIdx];
+            rightArrIdx += 1;
+            sortedArrIdx += 1;
+        }
+    }
+    
+    public static void quickSort(int[] arr) {
+        quickSortWirth(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSortWirth(int arr[], int l, int r) {
+        int i, j, w, x;
+        i = l;
+        j = r;
+        x = arr[(l + r) / 2];
+        do {
+            while (arr[i] < x)
+                i++;
+            while (x < arr[j])
+                j--;
+            if (i <= j) {
+                w = arr[i];
+                arr[i] = arr[j];
+                arr[j] = w;
+                i++;
+                j--;
+            }
+        } while (i <= j);
+        if (l < j)
+            quickSortWirth(arr, l, j);
+        if (i < r)
+            quickSortWirth(arr, i, r);
     }
 }
